9124aa7c5cce9f9535dd2614fa9cffba4a3488d0
Vladyslav Shevchenko <Vladyslav_Shevchenko@epam.com>
2017-09-13 11:50:03 +0300

    testing

--- pytest.py
+++ pytest.py
@@ -4,6 +4,6 @@ import sys
 
 if len(sys.argv) == 2:
 	docs = sys.argv[1].split('\n')
-	for i in docs: print(i.strip("+++ b/"))
+	for i in docs: print(i.strip("b/"))
 else:
 	print("No documents were added with this commit")
