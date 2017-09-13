7422863fb4c5a1448fe8701329f5cd9ec712d3c8
Vladyslav Shevchenko <Vladyslav_Shevchenko@epam.com>
2017-09-13 11:45:36 +0300

    added new functionality and document

--- pytest.py
+++ pytest.py
@@ -1,3 +1,9 @@
-import os
-docs = [i for i in os.listdir(".") if i.__contains__(".doc")] # returns list of docs in dir
-for i in docs: print(i + '\n')
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) == 2:
+	docs = sys.argv[1].split('\n')
+	for i in docs: print(i.strip("+++ b/"))
+else:
+	print("No documents were added with this commit")
