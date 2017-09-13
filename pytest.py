1dcf02531b9f8ee6d7f2be022cb62ab04bf89f3f
Vladyslav Shevchenko <Vladyslav_Shevchenko@epam.com>
2017-09-13 12:56:10 +0300

    added readability of files

--- pytest.py
+++ pytest.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
 import sys
+from docx import Document
 
 if len(sys.argv) > 1:
-	for i in sys.argv[1:]: print(i.strip("b/"))
+    print("File name: ", end='')
+    for i in sys.argv[1:]:
+        print(i)
+        document = Document(i)
+        fullText = []
+        [fullText.append(p.text) for p in document.paragraphs]
+        print('\nContents:\n' + '\n'.join(fullText))
 else:
 	print("No documents were added with this commit")
