5f1d30f5982920daae8b1fcca34537bdd78f38ce
Vladyslav Shevchenko <Vladyslav_Shevchenko@epam.com>
2017-09-13 12:05:36 +0300

    new logic test

--- pytest.py
+++ pytest.py
@@ -2,8 +2,7 @@
 
 import sys
 
-if len(sys.argv) == 2:
-	docs = sys.argv[1].split('\n')
-	for i in docs: print(i.strip("b/"))
+if len(sys.argv) > 1:
+	for i in sys.argv[1:]: print(i.strip("b/"))
 else:
 	print("No documents were added with this commit")
