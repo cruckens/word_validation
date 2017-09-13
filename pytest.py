a2ff4fa82ca1f02abc9b9d3474a2f212db4d99c0
Vladyslav Shevchenko <Vladyslav_Shevchenko@epam.com>
2017-09-13 13:00:50 +0300

    refactored printing

--- pytest.py
+++ pytest.py
@@ -4,9 +4,8 @@ import sys
 from docx import Document
 
 if len(sys.argv) > 1:
-    print("File name: ", end='')
     for i in sys.argv[1:]:
-        print(i)
+        print("File name: " + str(i))
         document = Document(i)
         fullText = []
         [fullText.append(p.text) for p in document.paragraphs]
